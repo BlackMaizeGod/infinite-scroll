d69d8cc2c7427a18efb0608b6c035d0125e63808
Artem Logvynenko <artuom.logv@gmail.com>
2020-10-03 16:07:16 +0300

    Initial commit

--- registration.php
+++ registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Chdtu_InfiniteScroll', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'BMG_InfiniteScroll', __DIR__);
